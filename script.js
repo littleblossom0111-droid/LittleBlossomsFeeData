65fd632b2497c66ed295c33c1a8c9999a6d4a76f
littleblossom0111-droid <littleblossom0111@gmail.com>
2026-08-05 12:03:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let students = JSON.parse(localStorage.getItem("students")) || [];
 displayStudents();
 
 
+// Save Student
+
 function saveStudent(){
 
 let student = {
@@ -42,30 +44,57 @@ location.reload();
 
 
 
+// Display Student
+
 function displayStudents(){
 
 let data="";
 
-students.forEach(function(s){
+
+students.forEach(function(s,index){
+
 
 data += `
 
 <tr>
+
 <td>${s.id}</td>
+
 <td>${s.name}</td>
+
 <td>${s.mobile}</td>
+
 <td>₹${s.monthlyfee}</td>
+
+<td class="action">
+
+<button class="edit" onclick="editStudent(${index})">
+✏️ Edit
+</button>
+
+
+<button class="delete" onclick="deleteStudent(${index})">
+🗑️ Delete
+</button>
+
+</td>
+
+
 </tr>
 
 `;
 
 });
 
+
 document.getElementById("studentList").innerHTML=data;
+
 
 }
 
 
+
+// Search Student
 
 function searchStudent(){
 
@@ -74,26 +103,69 @@ let value=document.getElementById("search").value.toLowerCase();
 let data="";
 
 
-students.filter(function(s){
+students.forEach(function(s,index){
 
-return s.name.toLowerCase().includes(value);
 
-}).forEach(function(s){
+if(s.name.toLowerCase().includes(value)){
+
 
 data += `
 
 <tr>
+
 <td>${s.id}</td>
+
 <td>${s.name}</td>
+
 <td>${s.mobile}</td>
+
 <td>₹${s.monthlyfee}</td>
+
+<td class="action">
+
+<button class="edit" onclick="editStudent(${index})">
+✏️ Edit
+</button>
+
+
+<button class="delete" onclick="deleteStudent(${index})">
+🗑️ Delete
+</button>
+
+</td>
+
+
 </tr>
 
 `;
+
+}
+
 
 });
 
 
 document.getElementById("studentList").innerHTML=data;
+
+
+}
+
+
+
+// Edit (अगले स्टेप में पूरा करेंगे)
+
+function editStudent(index){
+
+alert("Edit Feature Next Step Me Add Hoga");
+
+}
+
+
+
+// Delete (अगले स्टेप में Password के साथ जोड़ेंगे)
+
+function deleteStudent(index){
+
+alert("Delete Feature Next Step Me Add Hoga");
 
 }
