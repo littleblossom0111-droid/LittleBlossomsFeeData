aff3e44477404b632d46d0b06ce4b46aa36883df
littleblossom0111-droid <littleblossom0111@gmail.com>
2026-08-05 11:58:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,94 +1,77 @@
 let students = JSON.parse(localStorage.getItem("students")) || [];
 
+displayStudents();
 
-// Student Save
+
 function saveStudent(){
 
 let student = {
 
-id: "DC" + String(students.length + 1).padStart(3,"0"),
+id:"DC" + String(students.length + 1).padStart(3,"0"),
 
-name: document.getElementById("name").value,
+name:document.getElementById("name").value,
 
-father: document.getElementById("father").value,
+father:document.getElementById("father").value,
 
-mother: document.getElementById("mother").value,
+mother:document.getElementById("mother").value,
 
-mobile: document.getElementById("mobile").value,
+mobile:document.getElementById("mobile").value,
 
-address: document.getElementById("address").value,
+address:document.getElementById("address").value,
 
-admission: document.getElementById("admission").value,
+admission:document.getElementById("admission").value,
 
-dob: document.getElementById("dob").value,
+dob:document.getElementById("dob").value,
 
-regfee: document.getElementById("regfee").value,
+regfee:document.getElementById("regfee").value,
 
-monthlyfee: document.getElementById("monthlyfee").value
+monthlyfee:document.getElementById("monthlyfee").value
 
 };
 
 
 students.push(student);
 
-localStorage.setItem("students", JSON.stringify(students));
-
+localStorage.setItem("students",JSON.stringify(students));
 
 alert("Student Saved Successfully");
 
-
-window.location.reload();
+location.reload();
 
 }
 
 
 
-// Student List Show
-
 function displayStudents(){
 
-let table="";
-
+let data="";
 
 students.forEach(function(s){
 
-table += `
+data += `
 
 <tr>
-
 <td>${s.id}</td>
-
 <td>${s.name}</td>
-
 <td>${s.mobile}</td>
-
 <td>₹${s.monthlyfee}</td>
-
 </tr>
 
 `;
 
 });
 
-
-let list=document.getElementById("studentList");
-
-if(list){
-list.innerHTML=table;
-}
+document.getElementById("studentList").innerHTML=data;
 
 }
 
 
-
-// Search Student
 
 function searchStudent(){
 
 let value=document.getElementById("search").value.toLowerCase();
 
-
-let table="";
+let data="";
 
 
 students.filter(function(s){
@@ -97,19 +80,13 @@ return s.name.toLowerCase().includes(value);
 
 }).forEach(function(s){
 
-
-table +=`
+data += `
 
 <tr>
-
 <td>${s.id}</td>
-
 <td>${s.name}</td>
-
 <td>${s.mobile}</td>
-
 <td>₹${s.monthlyfee}</td>
-
 </tr>
 
 `;
@@ -117,42 +94,6 @@ table +=`
 });
 
 
-document.getElementById("studentList").innerHTML=table;
-
-
-}
-
-
-// Dashboard Update
-
-function updateDashboard(){
-
-let totalStudents = students.length;
-
-
-let totalRegistration = 0;
-
-
-students.forEach(function(s){
-
-totalRegistration += Number(s.regfee || 0);
-
-});
-
-
-if(document.getElementById("students"))
-document.getElementById("students").innerHTML=totalStudents;
-
-
-if(document.getElementById("registration"))
-document.getElementById("registration").innerHTML="₹"+totalRegistration;
-
+document.getElementById("studentList").innerHTML=data;
 
 }
-
-
-// Page Load
-
-displayStudents();
-
-updateDashboard();
